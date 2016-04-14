2cd32c27a0b4cd3c28c74372534fc8c95b08c5e8
student <225985@student.pwr.wroc.pl>
2016-04-14 12:23:28 +0200

    Rozwiązanie

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
 	g++ main.cpp -o lab0
 	./lab0 10 f
-	./lab0 5 d
+	./lab0 5 g
--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv)
+long int Fibonacci (int n)
 {
-	printf("Hello, World!\n");
+	if(n > 2)
+		return Fibonacci(n-1)+Fibonacci(n-2);
+	else
+		return 1;
+}
+
+long int Geometric (int n)
+{
+	if(n > 1)
+		return 4*Geometric(n-1);
+	else
+		return 1;
+}
+
+int main (int argc, char* argv[])
+{
+	int n = atoi(argv[1]);
+
+	if(argv[2][0] == 'f')
+	{
+		for(int i = 1; i <= n; i++)
+		{
+			long int number = Fibonacci(i);
+			printf("%ld\n", number);
+		}
+	}
+
+	else if(argv[2][0]=='g')
+	{
+		for(int i = 1; i <= n; i++)
+		{
+			long int number = Geometric(i);
+			printf("%ld\n", number);
+		}
+	}
 }
